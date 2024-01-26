41716615527cd69327e6e5f9502bf29ee0fbd697
jeweler23 <lopatin.lopatin2011@gmail.com>
2024-01-26 23:46:38 +0300

    feat: create two files

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import { ref, onMounted } from 'vue'
 import { API_KEY, API_URL } from './const/index.js'
 import WeatherSummery from './components/WeatherSummery.vue'
 import HighLight from './components/HighLight.vue'
+import Coords from './components/Coords.vue'
+import Humidity from './components/Humidity.vue'
 
 const city = ref('Moscow')
 const weatherInfo = ref(null)
 
 function getWeather() {
-  fetch(`${API_URL}q=${city.value}&appid=${API_KEY}`)
+  fetch(`${API_URL}q=${city.value}&units=metric&appid=${API_KEY}`)
     .then((response) => response.json())
     .then((data) => (weatherInfo.value = data))
 }
@@ -27,55 +29,16 @@ onMounted(getWeather)
                 <div class="city-inner">
                   <input v-model="city" type="text" class="search" @keyup.enter="getWeather" />
                 </div>
-                <WeatherSummery></WeatherSummery>
+                <WeatherSummery :weatherInfo="weatherInfo"></WeatherSummery>
               </div>
             </section>
             <section class="section section-right">
               <HighLight></HighLight>
             </section>
           </div>
-          <div class="sections">
-            <section class="section-bottom">
-              <div class="block-bottom">
-                <div class="block-bottom-inner">
-                  <div class="block-bottom-pic pic-coords"></div>
-                  <div class="block-bottom-texts">
-                    <div class="block-bottom-text-block">
-                      <div class="block-bottom-text-block-title">Longitude: 2.3488</div>
-                      <div class="block-bottom-text-block-desc">
-                        Longitude measures distance east or west of the prime meridian.
-                      </div>
-                    </div>
-                    <div class="block-bottom-text-block">
-                      <div class="block-bottom-text-block-title">Latitude: 48.8534</div>
-                      <div class="block-bottom-text-block-desc">
-                        Latitude lines start at the equator (0 degrees latitude) and run east and
-                        west, parallel to the equator.
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-            <section class="section-bottom">
-              <div class="block-bottom">
-                <div class="block-bottom-inner">
-                  <div class="block-bottom-pic pic-humidity"></div>
-                  <div class="block-bottom-texts">
-                    <div class="block-bottom-text-block">
-                      <div class="block-bottom-text-block-title">Humidity: 60 %</div>
-                      <div class="block-bottom-text-block-desc">
-                        Humidity is the concentration of water vapor present in the air. Water
-                        vapor, the gaseous state of water, is generally invisible to the human eye.
-                        <br /><br />
-                        The same amount of water vapor results in higher relative humidity in cool
-                        air than warm air.
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
+          <div class="sections" v-if="weatherInfo?.weather">
+            <Coords :coord="weatherInfo.coord"></Coords>
+            <Humidity ></Humidity>
           </div>
         </div>
       </div>
